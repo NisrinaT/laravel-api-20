d26c1c9eee8454db9c2ae69edadf1207eb7a4836
NisrinaT <akeerarael@gmail.com>
2026-08-03 00:34:22 +0700

    add POST and PUT method

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
 Route::get('/products', [ProductController::class, 'index'])->name('product');
+Route::post('/products', [ProductController::class, 'store'])->name('product.store');
+Route::put('/products/(product)', [ProductController::class, 'update'])->name('product.update');
