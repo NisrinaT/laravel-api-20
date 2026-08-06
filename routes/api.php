edeb3201c9984f02efd13bbe0d963565af3b7df1
NisrinaT <akeerarael@gmail.com>
2026-08-06 08:04:43 +0700

    method delete

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\Api\ProductController;
 
 Route::get('/products', [ProductController::class, 'index'])->name('product');
 Route::post('/products', [ProductController::class, 'store'])->name('product.store');
-Route::put('/products/(product)', [ProductController::class, 'update'])->name('product.update');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
